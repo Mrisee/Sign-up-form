5b42e63c5f8fc9abbcd7428725f420a445cd1323
Nikita <xolodov00@yandex.ru>
2023-03-24 11:25:16 +0300

    easing+timings+bg

--- js/script.js
+++ js/script.js
@@ -13,12 +13,17 @@ var button2Hidden = document.querySelector(".column2__hide-button");
 button1.addEventListener("click", replace);
 button1Hidden.addEventListener("click", replace);
 
+function easeInOutQuart (t, b, c, d) {
+    if ((t /= d / 2) < 1) return c / 2 * t * t * t * t + b;
+    return -c / 2 * ((t -= 2) * t * t * t - 2) + b;
+}
+
 function replace() {
     if(!isReplaced) {
         content1.animate({
             visibility: "hidden",
             opacity: "0"
-        }, {duration: 200, fill: "forwards"});
+        }, {duration: 400, fill: "forwards"});
         content1Hidden.animate({
             visibility: "visible",
             opacity: "1"
@@ -28,22 +33,24 @@ function replace() {
             visibility: "hidden",
             opacity: "0"
         }, {duration: 0, 
-            delay:"270", 
+            delay:"240", 
             fill: "forwards"});
         content2Hidden.animate({
             visibility: "visible",
             opacity: "1"
         }, {duration: 0, 
-            delay:"270", 
+            delay:"240", 
             fill: "forwards"});
         column1.animate({
             left: "50%"
-        }, {duration: 600, 
-            fill: "forwards"});
+        }, {duration: 500, 
+            fill: "forwards",
+            easing: "cubic-bezier(0.37, 0, 0.63, 1)"});
         column2.animate({
             right: "50%"
-        }, {duration: 600, 
-            fill: "forwards"});
+        }, {duration: 500, 
+            fill: "forwards",
+            easing: "cubic-bezier(0.37, 0, 0.63, 1)"});
         isReplaced = true;
         }
     else if (isReplaced) {
@@ -61,22 +68,24 @@ function replace() {
             visibility: "visible",
             opacity: "1"
         }, {duration: 0, 
-            delay:"270", 
+            delay:"240", 
             fill: "forwards"});
         content2Hidden.animate({
             visibility: "hidden",
             opacity: "0"
         }, {duration: 0, 
-            delay:"270", 
+            delay:"240", 
             fill: "forwards"});
         column1.animate({
             left: "0px"
-        }, {duration: 600, 
-            fill: "forwards"});
+        }, {duration: 500, 
+            fill: "forwards",
+            easing: "cubic-bezier(0.37, 0, 0.63, 1)"});
         column2.animate({
             right: "0%"
-        }, {duration: 600, 
-            fill: "forwards"});
+        }, {duration: 500, 
+            fill: "forwards",
+            easing: "cubic-bezier(0.37, 0, 0.63, 1)"});
         isReplaced = false;
     }
     
